935b933c99c5194d7634eb1fbc4707e6ee0a48dc
Attila Kun <kun.attila.hun@gmail.com>
2024-04-24 23:12:15 +0700

    initial commit

---
+++ swiftuibug/swiftuibugApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuibugApp.swift
+//  swiftuibug
+//
+//  Created by Attila Kun on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftuibugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
