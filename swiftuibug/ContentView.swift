0e5448a9c7528ce34f99883f9f7b0b200e00568b
Attila Kun <kun.attila.hun@gmail.com>
2024-04-24 23:35:18 +0700

    minimum reproduction

--- swiftuibug/ContentView.swift
+++ swiftuibug/ContentView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var foo: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text("Click (try single and also double click) on this and then change to the textfield quickly and type something there. Repeat this a couple times until you notice that you can't type into the textfield anymore.")
+        .focusable()
+        .onKeyPress { _ in
+            return .handled
         }
-        .padding()
+        
+        TextField("Enter something:", text: $foo)
     }
 }
 
